d031828202519e71870d8141f427eacfdd24ea0a
gabrieldev525 <gvictor525.gv@gmail.com>
2021-12-07 10:00:58 -0300

    Created pizza detail acitivity layout

---
+++ app/src/main/java/com/unifor/br/pizzadelivery/views/PaymentStepOrderActivity.kt
@@ -0,0 +1,12 @@
+package com.unifor.br.pizzadelivery.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.unifor.br.pizzadelivery.R
+
+class PaymentStepOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.payment_step_finish_order)
+    }
+}
