54d7a4ee26ca9a46e6b9802a12492cdbdea02a7d
gabrieldev525 <gvictor525.gv@gmail.com>
2021-12-06 21:39:13 -0300

    Add address step order activity layout

---
+++ app/src/main/java/com/unifor/br/pizzadelivery/views/AddressStepOrderActivity.kt
@@ -0,0 +1,12 @@
+package com.unifor.br.pizzadelivery.views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.unifor.br.pizzadelivery.R
+
+class AddressStepOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.address_step_finish_order)
+    }
+}
